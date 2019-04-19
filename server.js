fcc33bed797a3975621b45159a4cc15fe831735a
Matthew Puettmann <mattpuettmann@Matthews-MacBook-Pro.local>
2019-04-18 18:04:24 -0600

    Commit 1 - server set up and listening

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('server is listening on port 3000')
+})
