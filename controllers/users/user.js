6426fbd5ae33cbe9413c200ff70563fc7dde0d85
Matthew Puettmann <mattpuettmann@Matthews-MacBook-Pro.local>
2019-04-18 18:44:40 -0600

    Commit 3 - added bootstrap

--- controllers/users/user.js
+++ controllers/users/user.js
@@ -3,8 +3,14 @@ const mongoose = require('mongoose');
 const router = express.Router();
 
 
+//INDEX:
 router.get('/', (req, res) => {
     res.render('./users/index.ejs');
+});
+
+
+router.get('/new', (req, res) => {
+    res.render('./users/new.ejs');
 });
 
 
