181a494c0974ef06f49bd1be9cf63d32421d2b4e
Sami Austin <samicaustin@gmail.com>
2019-04-20 15:32:13 -0600

    Express app connected and rendering home index

---
+++ controllers/photoController.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/photoModel');
+const Cat = require('../models/userModel');
+
+
+
+
+module.exports = router;
