b033b91c9a6767f59f980b1f5b6c5722970d1556
Alexandra Thornton <alexandrathornton@Alexandras-MacBook-Pro.local>
2019-04-22 07:18:51 -0600

    everything works in this two model app

--- controllers/users.js
+++ controllers/users.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/users/User');
+const Photo = require('../models/photos/Photo');
 
 // SHOW INDEX
 router.get('/', (req, res) => {
     // res.send('hi i am here!!!');
-    User.find({}, (err, foundAllUsers) => {
+    User.find({}, (err, usersFromTheDatabase) => {
         if(err){
             console.log(err);
             res.send(err);
         } else {
-            console.log(foundAllUsers);
-            res.render('users/index.ejs', {user: foundAllUsers});
+            console.log(usersFromTheDatabase);
+            res.render('users/index.ejs', {
+                usersOnTheTemplate: usersFromTheDatabase
+            });
         }
     })
 })
@@ -37,15 +40,17 @@ router.get('/new', (req, res) => {
 
 // DELETE
 
-router.delete('/:id', (req, res) => {
-    User.findByIdAndDelete(req.params.id, (err, deletedUser) => {
-        if(err){
-            console.log(err);
-            res.send(err);
-        } else {
-            console.log(deletedUser);
+router.delete('/:id', (req, res)=>{
+    User.findByIdAndDelete(req.params.id, (err, userFromTheDatabase)=>{
+        console.log(userFromTheDatabase);
+        Photo.deleteMany({
+            _id: {
+                $in: userFromTheDatabase.photos
+            }
+        }, (err, data)=>{
+            console.log(data);
             res.redirect('/users');
-        }
+        })
     })
 })
 
@@ -66,29 +71,29 @@ router.put('/:id', (req, res) => {
 
 
 router.get('/:id/edit', (req, res) => {
-    User.findById(req.params.id, (err, foundUser) => {
-        if(err){
-            console.log(err);
-            res.send(err);
-        } else {
-            res.render('users/edit.ejs', {user: foundUser});
-        }
+    User.findById(req.params.id, (err, userFromTheDatabase)=>{
+        res.render('users/edit.ejs', {
+            userOnTheTemplate: userFromTheDatabase
+        })
     })
-});
+})
+
 
 //SHOW JUST ONE
 
 router.get('/:id', (req, res) => {
-    User.findById(req.params.id, (err, foundUser) => {
+    User.findById(req.params.id)
+    .populate('photos')
+    .exec((err, userFromTheDatabase) => {
         if(err){
             console.log(err);
             res.send(err);
         } else {
-            console.log(foundUser);
-            res.render('users/show.ejs', {user: foundUser});
+            res.render('users/show.ejs', {
+                userOnTheTemplate: userFromTheDatabase
+        });
         }
-    })
+    })    
 })
-
 
 module.exports = router;
--- models/users/User.js
+++ models/users/User.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: {type: String, required: true}
+    username: {type: String, required: true},
+    photos: [{type: mongoose.Schema.ObjectId, ref: 'Photo'}]
 });
 
 const User = mongoose.model('User', userSchema);
