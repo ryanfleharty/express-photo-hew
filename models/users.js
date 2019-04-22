3f9645bebf36e1cb749087ed3cc5bc1b8e4fbdce
Adam Wolfman <adamwolfman@Adams-MBP.domain>
2019-04-21 19:28:15 -0600

    updated styling and UI

--- models/users.js
+++ models/users.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     name: {type: String, required: true, unique: true}, 
-    photos: [{type: mongoose.Schema.Types.ObjectId, ref: 'Photo'}]
+    photos: [{type: mongoose.Schema.Types.ObjectId, ref: 'Photo'}],
+    bio: String
 });
 
 const User = mongoose.model('User', userSchema);
