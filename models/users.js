94e29d986d9982617a037212e1b896e3025d05ad
Adam Wolfman <adamwolfman@Adams-MBP.domain>
2019-04-21 20:01:01 -0600

    updated user schema with img

--- models/users.js
+++ models/users.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     name: {type: String, required: true, unique: true}, 
     photos: [{type: mongoose.Schema.Types.ObjectId, ref: 'Photo'}],
-    bio: String
+    bio: String,
+    img: {type: String, default: 'https://www.bookmydesign.com/auth-image/medium/blank-user.png?resize=202%2C60'}
 });
 
 const User = mongoose.model('User', userSchema);
