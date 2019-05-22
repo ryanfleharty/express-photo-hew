5a1649a618a8598ba909d96e292e5deff098420c
Joe Luchini <joeluchini@Joes-MacBook-Pro.local>
2019-05-22 16:14:44 -0600

    HOMEWORK DONE

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -83,36 +83,6 @@ router.post('/', (req, res)=>{
   });
 });
 
-//failed attempt at put route without using async await 
-// router.put('/:id', (req, res)=>{
-//   console.log(req.body, 'this is req.body');
-//   Photo.findByIdAndUpdate(req.params.id, req.body, {new: true},(err, updatedPhoto)=>{
-//     console.log(req.params.id, 'req.params.id before');
-//     User.findOne({'photo': req.params.id}, (err, userFound) => {
-//       console.log(req.params, 'this is only req.params')
-//       if(userFound._id.toString() !== req.body.userId){
-//         console.log(userFound._id, 'foundUser')
-//         console.log(req.body.user, 'req.body.user')
-//         userFound.photo.remove(req.params.id);
-//         userFound.save((err, savedUserFound) => {
-//           User.findById(req.body.userId, (err, newUser) => {
-//             newUser.photo.push(updatedPhoto._id);
-//             newUser.save((err, savedNewUser) => {
-//               res.redirect('/photo/' + req.params.id)
-//               console.log('hitting else')
-//             })
-//           })
-//         })
-
-//       } else {
-//         res.redirect('/photo/' + req.params.id)
-//         console.log('hitting else')
-//       }
-//     })
-//   });
-// });
-
-
 
 //Photo update route
 router.put("/:id", async (req,res)=>{
@@ -136,65 +106,42 @@ router.put("/:id", async (req,res)=>{
 
 
 
-router.delete('/:id', async (req,res)=>{
+// router.delete('/:id', async (req,res)=>{
 
-    try{
+//     try{
 
-      const deletedPhoto = await Photo.findByIdAndRemove(req.params.id);
-      const foundUser = await User.findOne({
-          'photo': req.params.id
-      });
-      foundUser.photo.remove(req.params.id)
-      foundUser.save(foundUser.photo)
-      res.redirect('/photo');
-    }catch(err){
-        res.send(err)
-    }
-    
-})
-
-// router.delete('/:id', async (req, res)=>{
-//   // Delete the article, is the purpose of line 153
-//   try {
-//         // Once again we are defining both are database queries, that
-//         // are not dependant on each other (the results don't depend on each other)
-//         // and we just have to await Promise.all below for them to finish
-//         // so we can remove the article from the author? Why do we have to do this?
-//         const deletePhoto = Photo.findByIdAndRemove(req.params.id);
-//         const findUser    = User.findOne({'photoz': req.params.id});
-
-//         const [deletedPhoto, foundUser] = await Promise.all([deletePhoto, findUser]);
-
-//         // Why do we have to find the author
-//         // then remove the article from the foundAuthor document?
-//         // If we remove an article from the Article COllection,
-//         // does it automatically remove from the author collection?
-//         foundUser.photoz.remove(req.params.id);
-//         // Don't forget to save! the document back to the database
-//         await foundUser.save();
-//         console.log('happening ')
-//         res.redirect('/photo', {
-//         photoz: foundPhotos
-//         });
-
-//     } catch(err){
-
-//       console.log(err)
-//       res.send(err);
+//       const deletedPhoto = await Photo.findByIdAndRemove(req.params.id);
+//       const foundUser = await User.findOne({
+//           'photo': req.params.id
+//       });
+//       foundUser.photo.remove(req.params.id)
+//       foundUser.save(foundUser.photo)
+//       res.redirect('/photo');
+//     }catch(err){
+//         res.send(err)
 //     }
-// });
+    
+// })
 
+router.delete('/:id', (req, res) => {
 
-
-
-
-
-
-
-
-
-
-
-
+  Photo.findByIdAndDelete(req.params.id, (error, deletedPhotoFromDb) => {
+      if (error){
+          res.send(error)
+      } else {
+          User.findOne({'photo': req.params.id}, (error, foundUser) => {
+              if(error){
+                  res.send(error);
+              } else {
+                  foundUser.photos.remove(req.params.id);
+                  foundUser.save((error, updatedUser) => {
+                      console.log(updatedUser);
+                      res.redirect('/photo')
+                  })
+              }
+          })
+      }
+  });
+});
 
 module.exports = router;
